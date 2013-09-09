ea1512d0cea44eb0fb43ef9e9f69b3c1ebd0bb03
Kreistschen <csimon1990@googlemail.com>
2013-09-09 18:23:31 +0200

    Log Funktion eingebaut

--- Muehle_Master/src/de/dhbw/muehle/core/Core.java
+++ Muehle_Master/src/de/dhbw/muehle/core/Core.java
@@ -1,12 +1,14 @@
 package de.dhbw.muehle.core;
 
 import de.dhbw.muehle.gui.ViewController;
+import de.dhbw.muehle.model.Log;
 import de.dhbw.muehle.model.Model;
 
 public class Core {
 
 	ViewController vController;
 	Model model;
+	Log log;
 	
 	public Core(){
 		vController = new ViewController(this);
@@ -15,6 +17,11 @@ public class Core {
 
 	public void init(){
 		vController.init();
+		run();
+	}
+	
+	private void run(){
+		Log.log("run() ohne Fehler gestartet",getClass().getSimpleName());
 	}
 }
 
