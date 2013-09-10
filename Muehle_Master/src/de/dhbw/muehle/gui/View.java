40f64f0ed0506c6c2bdeec90c239df06d4835aa8
Ammon93 <Ammon@141.31.70.96>
2013-09-10 08:49:08 +0200

    Probleme mit joda-time Library behoben Befindet sich nun im Projektordner unter lib/

--- Muehle_Master/src/de/dhbw/muehle/gui/View.java
+++ Muehle_Master/src/de/dhbw/muehle/gui/View.java
@@ -1,13 +1,11 @@
 package de.dhbw.muehle.gui;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 
 public class View {
 	
 	private JFrame frame;
-	private JLabel label;
 	
 	public void init(){
 		//System.out.println("Passt");
