1a9276faa51c6591eb9cd8c63a3d8b7ce012f56f
Kreistschen <csimon1990@googlemail.com>
2013-09-18 13:58:19 +0200

    erzeugeSpielsteine Methode hinzugefügt

--- Muehle_Master/src/de/dhbw/muehle/core/Core.java
+++ Muehle_Master/src/de/dhbw/muehle/core/Core.java
@@ -3,6 +3,9 @@ package de.dhbw.muehle.core;
 import de.dhbw.muehle.gui.ViewController;
 import de.dhbw.muehle.model.Log;
 import de.dhbw.muehle.model.Model;
+import de.dhbw.muehle.model.spielstein.EPositionIndex;
+import de.dhbw.muehle.model.spielstein.ESpielsteinFarbe;
+import de.dhbw.muehle.model.spielstein.Spielstein;
 
 public class Core {
 	
@@ -18,11 +21,29 @@ public class Core {
 
 	public void initGame(){
 		vController.initGui();
-		run();
+		erzeugeSpielsteine();
+		
 	}
 	
 	private void run(){
 		Log.log("run() ohne Fehler gestartet",getClass().getSimpleName());
+	}
+	
+	private void erzeugeSpielsteine(){
+		
+		Spielstein [] StW = new Spielstein [9];
+		Spielstein [] StS = new Spielstein [9];
+		
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				StW[j]= new Spielstein(EPositionIndex.Drei,EPositionIndex.Drei,EPositionIndex.Drei,ESpielsteinFarbe.WEISS);
+			}
+		StS[i]= new Spielstein(EPositionIndex.Drei,EPositionIndex.Drei,EPositionIndex.Drei,ESpielsteinFarbe.WEISS);
+		
+		}
+	
+		Log.log("Alle Spielsteine wurden erstellt",getClass().getSimpleName());
+		
 	}
 }
 
